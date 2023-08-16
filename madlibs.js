45f35d02a5b86be7ac03a31f3d293a893682b332
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-08-17 00:12:26 +0100

    Update madlibs.js

--- madlibs.js
+++ madlibs.js
@@ -1,92 +1,55 @@
-/**
- * Complete the implementation of parseStory.
- *
- * parseStory retrieves the story as a single string from story.txt
- * (I have written this part for you).
- *
- * In your code, you are required (please read this carefully):
- * - to return a list of objects
- * - each object should definitely have a field, `word`
- * - each object should maybe have a field, `pos` (part of speech)
- *
- * So for example, the return value of this for the example story.txt
- * will be an object that looks like so (note the comma! periods should
- * be handled in the same way).
- *
- * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
- * Output: [
- *  { word: "Louis", pos: "noun" },
- *  { word: "went", pos: "verb", },
- *  { word: "to", },
- *  { word: "the", },
- *  { word: "store", pos: "noun" }
- *  { word: "," }
- *  ....
- *
- * There are multiple ways to do this, but you may want to use regular expressions.
- * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
- */
 function parseStory(rawStory) {
   console.log(rawStory);
   // const random = rawStory.replaceAll(",", " ,").replaceAll(".", " .").split(" ");
   // console.log (random);
-  
-  const parsedStory = rawStory.match(/[a-zA-Z]+\[v\]|[a-zA-Z]+\[n\]|[a-zA-Z]+\[a\]|[a-zA-Z]+|[,\.]/g);
-  console.log(parsedStory);
+
+  const parsedStory = rawStory.match(
+    /[a-zA-Z]+\[v\]|[a-zA-Z]+\[n\]|[a-zA-Z]+\[a\]|[a-zA-Z]+|[,\.]/g
+  );
+  // console.log(parsedStory);
 
   const parsedObjectsStory = parsedStory.map((element) => {
     if (element.endsWith("[n]") === true) {
-      return {word:element.slice(0, -3) , pos: "noun"};
+      return { word: element.slice(0, -3), pos: "noun" };
     } else if (element.endsWith("[v]") === true) {
-      return {word:element.slice(0, -3) , pos: "verb"};
+      return { word: element.slice(0, -3), pos: "verb" };
     } else if (element.endsWith("[a]") === true) {
-      return {word:element.slice(0, -3) , pos: "adjective"};
+      return { word: element.slice(0, -3), pos: "adjective" };
     } else {
-      return {word:element};
+      return { word: element };
     }
-
   });
   return parsedObjectsStory;
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- *
- * You'll want to use the results of parseStory() to display the story on the page.
- */
-
-
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
-
-
     // we get the components from the html
     const madLibsEdit = document.querySelector(".madLibsEdit");
     const madLibsPreview = document.querySelector(".madLibsPreview");
-    
-    // create 
-    const text = [];
-    
+
+    // create
+    // const text = [];
+
     // function that creates input field
     function createInput(index, defaultValue) {
       const input = document.createElement("input");
       input.setAttribute("data-index", index);
-      input.placeholder = defaultValue || "___"
+      input.placeholder = defaultValue || "___";
+      input.maxLength = 20;
       // input.value = "";
-      input.addEventListener("input", updatePreview);
+      input.addEventListener("input", () => updatePreview(processedStory));
       return input;
     }
-    //This is a comment
-    // this is comment 2
+
     // function that creates empty space
     function createSpan(text) {
       const span = document.createElement("span");
-      span.textContent = text ? text + " " : "___ "; // Display underscores if text is empty
+      span.textContent = text ?  text+" "   : "___ "; // Display underscores if text is empty
       return span;
     }
-  
+
     // generate the madlibs
     function generateMadLibs(array) {
       array.forEach((word, index) => {
@@ -94,26 +57,24 @@ getRawStory()
           const input = createInput(index, word.pos);
           madLibsEdit.appendChild(input);
         } else {
-          text.push(word.word);
+         // text.push(word.word);
           const span = createSpan(word.word);
           madLibsEdit.appendChild(span);
         }
       });
     }
-    
-    generateMadLibs(processedStory);
-    
-    
+
     // function to update the preview
-    function updatePreview() {
+    function updatePreview(array) {
       while (madLibsPreview.firstChild) {
         madLibsPreview.removeChild(madLibsPreview.firstChild);
       }
-    
-      processedStory.forEach((word) => {
-       
+
+      array.forEach((word) => {
         if (word.pos) {
-          const input = madLibsEdit.querySelector(`input[data-index="${processedStory.indexOf(word)}"]`);
+          const input = madLibsEdit.querySelector(
+            `input[data-index="${array.indexOf(word)}"]`
+          );
           const span = createSpan(input.value);
           madLibsPreview.appendChild(span);
         } else {
@@ -122,4 +83,7 @@ getRawStory()
         }
       });
     }
-  })
+
+    generateMadLibs(processedStory);
+    updatePreview(processedStory);
+  });
